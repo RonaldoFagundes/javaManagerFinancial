10374e61b64ff65f33128162354d9d1c181ae703
RonaldoFagundes <ronaldofagundes@gmail.com>
2023-06-17 15:59:04 -0300

    upload credencial

--- javaManagerFinamcial/src/javaManagerFinamcial/Main.java
+++ javaManagerFinamcial/src/javaManagerFinamcial/Main.java
@@ -3,7 +3,7 @@ package javaManagerFinamcial;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("java app financial net beans");
+		System.out.println("java app financial net beans credenciais");
 	}
 
 }
