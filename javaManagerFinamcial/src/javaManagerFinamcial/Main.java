c9ac16f9a2190680590162f656c109d2cb1e14ce
RonaldoFagundes <ronaldofagundes@gmail.com>
2023-06-17 17:01:13 -0300

    upload of files

--- javaManagerFinamcial/src/javaManagerFinamcial/Main.java
+++
@@ -1,9 +0,0 @@
-package javaManagerFinamcial;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println(" upload java app financial net beans credenciais do token");
-	}
-
-}
